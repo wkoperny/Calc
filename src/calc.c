eb924f425660c36cd43fde6519661852595ed2b2
wkoperny <wojciech.koperny@gmail.com>
2018-03-01 22:32:21 +0100

    added multiply

--- src/calc.c
+++ src/calc.c
@@ -4,3 +4,8 @@ uint32_t Calculator_Add(uint32_t firstNumber, uint32_t secondNumber)
 {
 	return firstNumber + secondNumber;
 }
+
+uint32_t Calculator_Multiply(uint32_t firstNumber, uint32_t secondNumber)
+{
+	return firstNumber * secondNumber;
+}
--- src/calc.h
+++ src/calc.h
@@ -3,5 +3,5 @@
 //#include <stdint.h>
 
 uint32_t Calculator_Add(uint32_t firstNumber, uint32_t secondNumber);
-
+uint32_t Calculator_Multiply(uint32_t firstNumber, uint32_t secondNumber);
 #endif /* CALCULATOR_H_ */
