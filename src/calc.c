e2b7ff35efbe87cc130e2fb16d8ce7e8e00372fa
wkoperny <wojciech.koperny@gmail.com>
2018-03-01 22:42:46 +0100

    dodano komentarze

--- src/calc.c
+++ src/calc.c
@@ -1,10 +1,10 @@
 #include "Calc.h"
-
+//ta funkcja dodaje
 uint32_t Calculator_Add(uint32_t firstNumber, uint32_t secondNumber)
 {
 	return firstNumber + secondNumber;
 }
-
+//ta funkcja mnozy
 uint32_t Calculator_Multiply(uint32_t firstNumber, uint32_t secondNumber)
 {
 	return firstNumber * secondNumber;
